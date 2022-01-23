32a04cb385e2936d6f574d69d086cc7b164e5a4e
sameer nyapu <94948209+nyapu@users.noreply.github.com>
2022-01-23 05:02:54 -0500

    if else

---
+++ if_Else conditions&switch.js
@@ -0,0 +1,26 @@
+console.log('if else condition');
+
+const age=17;
+const doesdrive=true;
+// if(age==17)
+// {
+//     console.log('Age is 17');
+// }
+// if(age==34){
+//     console.log('Age is 34');
+// }
+// else{
+//     console.log('Age is not 17');
+// }
+
+
+// if(doesdrive && age>16){
+//     console.log('you can drive');
+// }
+// else{
+//     console.log('you cannot drive');
+// }
+  
+
+//*** torneror operator */
+console.log(age==17? 'Age is 17':'Age is not 17');
