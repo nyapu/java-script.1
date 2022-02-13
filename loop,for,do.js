b62c35d53b048479d9bd5f3894c9a575a1854058
sameer nyapu <94948209+nyapu@users.noreply.github.com>
2022-02-12 16:11:46 -0800

    loop

---
+++ loop,for,do.js
@@ -0,0 +1,43 @@
+console.log('i am in tutorial 9');
+// General loop:for,while and do-while;
+
+// const a=0;
+// a +=1;
+// console.log(a)
+
+// for (let i = 0; i < 10; i++) {
+   
+//     console.log(i);
+// }
+//WHILE LOOP
+// let a=0;
+// while (a<100) {
+//     console.log(a);
+//     a++;
+    
+// }
+//DO WHILE LOOP
+// let j=0;
+// do {
+//     console.log(j);
+//     j++;
+// } while (j<10);
+// let g=0;
+// do{
+// console.log(g);
+// if(g===5){
+//     break;
+// }
+//     g++;
+// }while(g<10);
+
+
+
+// let arr=[2,3,5,6,7,2,3];
+// arr.forEach(function(element){
+//     console.log(element);
+// });
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
