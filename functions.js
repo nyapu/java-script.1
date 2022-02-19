94eee627d492a7a9aad0c30c7f850994ee12e5ec
sameer nyapu <94948209+nyapu@users.noreply.github.com>
2022-02-18 21:04:48 -0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,64 @@
+console.log('i am in tutorial 10');
+/* let name ='rohan';
+ let name1='sudip';
+ console.log(`${name} May you be gifted with life’s biggest 
+ joys and never-ending bliss. After all, you 
+ yourself are a gift to earth,
+  so you deserve the best. Happy birthday.`);
+ 
+  console.log(`${name1} May you be gifted with life’s biggest 
+ joys and never-ending bliss. After all, you 
+yourself are a gift to earth,
+ so you deserve the best. Happy birthday.`);
+ */
+
+/*
+
+
+function greet(name,thank='Thanks a lots') {
+    console.log(`${name} May you be gifted with life’s biggest 
+ joys and never-ending bliss. After all, you 
+ yourself are a gift to earth,
+  so you deserve the best. Happy birthday. ${thank}`);
+return 5;
+}
+let name = 'sudip';
+let name1 = 'Rohan';
+// greet(name);
+
+let val=greet(name1);
+console.log(val);
+*/
+/*
+// variable as a functions
+const mygreet =function(name){
+    console.log(`hello world ${name}`)
+}
+let name='hari';
+mygreet(name);
+*/
+/*
+const myobj={
+    name:'sudip',
+    game:function(){
+        return "GTA";
+    }
+}
+console.log(myobj.game());
+
+
+arry=['fruit','vegetable','funiutre'];
+arry.forEach(function(element,index,array){
+    console.log(element,index)
+});*/
+
+
+let i = 456;
+console.log(i);
+function ui(name) {
+    let i = 9;
+    console.log(i);
+    return `This is a ${name} ui`;
+
+}
+console.log(ui("sudip"), i);
