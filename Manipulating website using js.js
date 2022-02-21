b52cb7a2aa91524195a25188cd755502386aeeb8
sameer nyapu <94948209+nyapu@users.noreply.github.com>
2022-02-21 06:47:35 +0545

    Add files via upload

---
+++ Manipulating website using js.js
@@ -0,0 +1,21 @@
+console.log('i am in tutorial 11');
+// let a=window.document;
+let a=window;
+// window.alert('this page is vulnerable');
+// a=prompt('i will donate bitcoin plz enter your valid bit coin address');
+
+// a=confirm('you want to delete this page or not?');
+// a=window.innerHeight;
+// a=window.innerWidth;
+//a=window.scrollx;(window is global object thats why it is not necessary to write in code)
+// a=scrollX;
+// a=scrollY;
+// a=location;
+// location.reload
+// location.href
+//location.href=code with harry;
+//location.href='//nepal.com';
+// a=window.history;
+//history.go(-1)
+
+console.log(a);
