776d1da5821de1ccd3e3bf6a80b1c8955f286ef7
sameer nyapu <94948209+nyapu@users.noreply.github.com>
2022-02-22 17:57:44 +0545

    Add files via upload

---
+++ html element selectors in js.js
@@ -0,0 +1,24 @@
+console.log('i am in tutorial 14');
+/*
+element selectors:
+1 single element selector
+2 multi element selecotr
+*/
+
+// 1 single element selecotr
+// let element=document.getElementById('apple');
+// element=element.className;
+// element=element.childNodes;
+// element=element.parentNode;
+// element.style.color='green';
+// element.innerText='nyapu hacker';
+// element.innerHTML=' <h1>you</h1>';
+// console.log(element);
+
+// let sel=document.querySelector('#apple');
+// sel=document.querySelector('.myfirst');
+// sel=document.querySelector('div');
+// sel.style.color='green';
+// console.log(sel);
+
+//2 Multi element selector
