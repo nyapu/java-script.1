32895fd3d98d397bc06daf1f7c5c63ff42950598
sameer nyapu <94948209+nyapu@users.noreply.github.com>
2022-02-22 07:13:58 +0545

    Add files via upload

---
+++ understanding  Dom and creating  website layout.js
@@ -0,0 +1,17 @@
+console.log('i am in tutorial 12');
+
+// a=document;
+// a=document.all;
+// a=document.body;
+// a=document.forms;
+
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// })
+// console.log(a);
+// a=document.links[0];
+// console.log(a)
+
+ a=document.images;
+// a=document.scripts;
+console.log(a);
