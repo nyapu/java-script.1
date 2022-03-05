e392c4bbfbdb3048edc7063d0b017bfdba45a9c6
sameer nyapu <94948209+nyapu@users.noreply.github.com>
2022-03-05 08:23:05 +0545

    Add files via upload

--- webpage crawler.js
+++ webpage crawler.js
@@ -11,12 +11,39 @@ javascript*/
 
 
 /*  SOLUTION BY SAMEER TIRUWA*/
-Z=document.links;
+// Z=document.links;
 
-let a='Harry';
-let b ='javascript';
- Array.from(Z).forEach(function(element){
-     if(String(element).includes(b)){
-         console.log(element)
-     };
- });
+// let a='Harry';
+// let b ='javascript';
+//  Array.from(Z).forEach(function(element){
+//      if(String(element).includes(b)){
+//          console.log(element)
+//      };
+//  });
+
+
+
+/// console.log(i am in tutorial 19)
+/*
+let str="python";
+let links=document.links;
+console.log(links);
+let href;
+Array.from(links).forEach(function(element){
+href=element.href;
+console.log(href);
+});
+*/
+/*
+
+let str="python";
+let links=document.links;
+console.log(links);
+let href;
+Array.from(links).forEach(function(element){
+href=element.href;
+if(href.includes(str)){
+    console.log(href);
+}
+
+});
